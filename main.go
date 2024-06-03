3df896dc437769e84219dde01b8843a2ed90c47b
Yhya <yhyamobile57@gmail.com>
2024-06-03 22:49:13 +0300

    make a gofiber app

--- main.go
+++ main.go
@@ -1,3 +1,14 @@
 package main
 
-func main() {}
+import (
+	"github.com/gofiber/fiber/v2"
+    "github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func main() {
+	app := fiber.New()
+
+    app.Use(cors.New())
+
+	app.Listen(":8080")
+}
